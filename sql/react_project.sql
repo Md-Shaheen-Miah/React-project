251eef6d19eb6f9e2e2784648f74db953a1c869a
mdshaheenmiah <mdshaheen60101@gmail.com>
2024-07-14 19:08:27 +0600

    update

--- sql/react_project.sql
+++ sql/react_project.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 13, 2024 at 08:15 AM
+-- Generation Time: Jul 14, 2024 at 03:05 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,36 @@ SET time_zone = "+00:00";
 --
 -- Database: `react_project`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `phone` varchar(55) DEFAULT NULL,
+  `checkin` timestamp NULL DEFAULT NULL,
+  `checkout` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `adults` int(11) DEFAULT NULL,
+  `children` int(11) DEFAULT NULL,
+  `room_no` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`id`, `name`, `email`, `phone`, `checkin`, `checkout`, `adults`, `children`, `room_no`) VALUES
+(22, 'iy7', 's@gmail.com', '2545', '2024-07-01 18:00:00', '2024-07-25 18:00:00', 2, 2, '455'),
+(23, 'iy7', 's@gmail.com', '2545', '2024-07-13 18:00:00', '2024-07-17 18:00:00', 2, 2, '455'),
+(24, 'raju', 'raju@gmail.com', '124578653', '2024-07-14 18:00:00', '2024-07-18 18:00:00', 2, 2, '102'),
+(25, 'sa', 's@gmail.com', '12455623', '2024-07-07 18:00:00', '2024-07-16 18:00:00', 2, 2, '4578'),
+(26, 'jamal', 'jamal@gmail.com', '12345678', '2024-07-15 18:00:00', '2024-07-21 18:00:00', 2, 1, '202'),
+(27, 'Rafiya khan', 'R@gmail.com', '2135254787897', '2024-07-15 18:00:00', '2024-07-17 18:00:00', 1, 1, '301');
 
 -- --------------------------------------------------------
 
@@ -43,11 +73,17 @@ CREATE TABLE `staffs` (
 --
 
 INSERT INTO `staffs` (`id`, `name`, `email`, `phone`, `address`, `position`, `department`, `salary`) VALUES
-(48, 'shaheen', 'shaheen@gmail.com', '01641160101', 'tongi', 'attendent', 'HK', 14500.00);
+(57, 'sakil', 'de@gmail.com', '4189', 'kjlg7', 'b', ' ftg', 214.00);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `staffs`
@@ -61,10 +97,16 @@ ALTER TABLE `staffs`
 --
 
 --
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
 -- AUTO_INCREMENT for table `staffs`
 --
 ALTER TABLE `staffs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
